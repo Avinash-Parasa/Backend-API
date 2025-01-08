4a26d23f374ddb9f0dadec59a9de39449dd30506
Avinash Parasa <avinashpa@Meditab.Local>
2025-01-08 14:16:13 +0530

    Your commit message describing the changes

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.use('/api/user', userRoutes);  // Use the user routes
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
